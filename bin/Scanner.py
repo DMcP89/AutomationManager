d5dd616000bca3657391a7aa1d53269f2358cc79
Dave <davemcpherson@wochstudios.com>
2016-04-21 20:30:40 -0400

    started to add task handling and scheduling

--- bin/Scanner.py
+++ bin/Scanner.py
@@ -28,7 +28,7 @@ def scanNetwork( mac, network ):
 				return results
 
 
-#test = scanNetwork('***REMOVED***')
+#test = scanNetwork('84:10:0D:6A:C7:BF')
 #print test
 
 
--- bin/SqliteHandler.py
+++ bin/SqliteHandler.py
@@ -9,7 +9,26 @@ def logConnection():
 	conn.commit
 	return
 
+def checkTask( task_name ):
+	cursor = conn.cursor()
+	cursor.execute("SELECT id,name,last_executed,result FROM tasks WHERE name = ?"\
+			,(task_name,))
+	for id, name, last_executed, result  in cursor:
+		print "id = ", id
+		print "name = ", name
+		print "last_executed = ",last_executed
+		print "result = ",result
+	return
+
+def updateTask():
+	return
+
+
 def closeDatabase():
 	conn.close()
 	return
 
+
+
+##TESTING##
+checkTask( "test" )
