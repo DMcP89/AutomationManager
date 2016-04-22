85b819add3a5855004f578c3345c67694a14af93
Dave <davemcpherson@wochstudios.com>
2016-04-21 20:30:40 -0400

    started to add task handling and scheduling

--- bin/Manager.py
+++ bin/Manager.py
@@ -10,29 +10,28 @@ import ConfigParser
 config = ConfigParser.ConfigParser()
 config.read("../conf/automation.ini")
 
-def init():
+def run():
 	logging.basicConfig(filename='../log/manager.log',level=logging.DEBUG)
 	logging.debug('Manager Start')
 	s = sched.scheduler(time.time,time.sleep)
-	s.enter(60,1,networkScan, ())
+	s.enter(60,1,networkScan, (s, ))
 	s.run()
 	return
 
 
-def networkScan():
+def networkScan( scheduler ):
 	logging.info('Scanning Network')
 	scan = Scanner.scanNetwork( mac=config.get('ScannerProperties','target'),
 					 network=config.get('ScannerProperties','network'))
 	if (scan == 1):
 		SqliteHandler.logConnection()
 		logging.info('Connection Logged')
-	
+	scheduler.enter(60,1,networkScan,(scheduler, ))
 	return
 
 
 def main():
-	init()
-	networkScan()
+	run()
 	logging.debug('Manager End \n')
 	return
 
